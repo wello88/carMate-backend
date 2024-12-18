e6f842e64f10274d90a8003864599b3319c2cce2
wello88 <abdow8896@gmail.com>
2024-12-19 01:13:10 +0200

    sign up & log in API , user model, worker model

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('database_name', 'username', 'password', {
+const sequelize = new Sequelize('carmate', 'postgres', '123', {
   host: 'localhost',
   dialect: 'postgres',
   logging: false, // Disable SQL logging
 });
 
-export default sequelize;
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1); // Exit the process on connection failure
+  }
+};
+
+export { sequelize, connectDB };
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-//import modules
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 import express from "express"
+import authRouter from "./src/modules/auth/auth.router.js"
 import path from "path"
 
 import { initApp } from "./src/initApp.js"
+import { syncDatabase } from "./db/index.js"
 
 const app = express()
 
+// app.use(express.json())
+// app.use('/auth',authRouter())
+
+syncDatabase();
+// authRouter()
 
 
-dotenv.config({ path: path.resolve('./config/.env') })
+// dotenv.config({ path: path.resolve('./config/.env') })
 initApp(app, express)
