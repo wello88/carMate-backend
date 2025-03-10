efceee593b7f8f173f7220b4822f1b7c4ea2c615
wello88 <abdow8896@gmail.com>
2025-03-10 03:30:25 +0200

    edit session model

--- db/models/session.model.js
+++ db/models/session.model.js
@@ -16,8 +16,8 @@ Session.init(
     postId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'posts', key: 'id' } },
     workerId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'workers', key: 'id' } },
     offerId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'offers', key: 'id' } },
-    startDate: { type: DataTypes.DATE, allowNull: true },
-    endDate: { type: DataTypes.DATE, allowNull: true },
+    startDate: { type: DataTypes.STRING, allowNull: true },
+    endDate: { type: DataTypes.STRING, allowNull: true },
     isDone: { type: DataTypes.BOOLEAN, defaultValue: false },
   },
   { sequelize, modelName: 'Session', tableName: 'sessions', timestamps: true }
