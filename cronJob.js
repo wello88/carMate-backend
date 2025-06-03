ec7fe62a1d1d95b887b63cc875f01946f0086863
wello88 <abdow8896@gmail.com>
2025-06-03 04:21:27 +0300

    26

--- cronJob.js
+++ cronJob.js
@@ -45,7 +45,7 @@ export const sendUpcomingDeadlineNotifications = async () => {
   }
 };
 
-cron.schedule('19 4 * * *', () => {
+cron.schedule('26 4 * * *', () => {
   console.log(`[${new Date().toISOString()}] Running daily reminder check at 3:39 AM…`);
   sendUpcomingDeadlineNotifications();
 });
